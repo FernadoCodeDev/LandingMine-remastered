c7122d3f55bc75012e11482f686845a555f93782
Fernando <fernando.e.mejia.e@gmail.com>
2025-11-02 11:53:27 -0600

    Pagination Adjustment Dream House

--- src/LandingPages/DreamHouse/utils/PaginationSection.jsx
+++ src/LandingPages/DreamHouse/utils/PaginationSection.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Paginator from "./Paginator";
-import Page1 from "./page1";
-import Page2 from "./page2";
-import Page3 from "./page3";
-import Page4 from "./page4";
+import Page1 from "./Page1";
+import Page2 from "./Page2";
+import Page3 from "./Page3";
+import Page4 from "./Page4";
 
 const PaginationSection = () => {
   return (
